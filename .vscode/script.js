7c8ca98cd18d774da0b0a42e703561978acfe520
william <williamp783@gmail.com>
2023-09-25 22:41:27 -0300

    first commit

---
+++ .vscode/script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //pegar a tag imagem
+  const img = document.querySelector("#profile img")
+  //substituir imagem
+  if(html.classList.contains('light')){
+    //se tiver light mode, adcionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    //se tiver sem light manter imagem atual
+    img.setAttribute('src','./assets/avatar.png')
+  }
+}
